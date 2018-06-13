8bb98990ef631df35609da521ee95bdddaeb752d
Olmos Soto Manuel Alejandro <MSOLMOS@entel.cl>
2018-06-13 13:12:56 -0400

    Beginning of exercises

---
+++ Ejercicio2.rb
@@ -0,0 +1,20 @@
+# Dado el arreglo
+nombres = ["Violeta", "Andino", "Clemente", "Javiera", "Paula", "Pia", "Ray"]
+
+Se pide:
+
+
+# 1. Extraer todos los elementos que excedan mas de 5 caracteres utilizando el método .select.
+
+
+# 2. Utilizando .map crear una arreglo con los nombres en minúscula.
+
+
+# 3. Utilizando .select para crear un arreglo con todos los nombres que empiecen con P.
+
+
+# 4. Utilizando .map crear un arreglo único con la cantidad de letras que tiene cada nombre.
+
+
+# 5. Utilizando .map y .gsub eliminar las vocales de todos los nombres.
+
