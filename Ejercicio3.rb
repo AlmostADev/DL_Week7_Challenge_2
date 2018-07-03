677d967fa8ca5ffc65013938d06af8a3f5ecb76f
Olmos Soto Manuel Alejandro <MSOLMOS@entel.cl>
2018-07-03 12:44:16 -0400

    Some changes

--- Ejercicio3.rb
+++ Ejercicio3.rb
@@ -1,50 +1,77 @@
+
+def averageAlumnos(data)
+    file2 = File.open('promedios.txt', 'w')
+        data.each do |item|
+            names = item.split(', ')[0]
+            marks = item.split(', ')[1..5].each.map(&:to_i)
+            avg = 0
+            marks.each do |mark|
+                avg += mark
+            end
+            promedio = avg / marks.length.to_f
+            file2.puts "#{names} : #{promedio}"
+        end
+    file2.close
+end
+
+def inasistenciasAlumnos(data)
+    data.each do |item|
+        names = item.split(', ')[0]
+        marks = item.split(', ')[1..5].each.map(&:to_i)
+        asistencia = marks.count(0)
+        puts "#{names} : #{asistencia} asistencia" if asistencia >= 1
+    end
+end
+
+def approveStudents(data, not_minima)
+    data.each do |item|
+        names = item.split(', ')[0]
+        marks = item.split(', ')[1..5].each.map(&:to_i)
+        avg = 0
+        marks.each do |mark|
+            avg += mark
+        end
+        promedio = avg / marks.length.to_f
+        puts "#{names} aprobado, Felicitaciones! :)" if promedio >= not_minima
+    end
+end
+
 # 1. Crear un menú que permita registrar la información de los alumnos del curso.
 puts "Bienvenido al Inventario de Desafio Latam..."
 puts "Reservando espacio en memoria..."
+
 persona = Hash.new()
 puts persona
+
 loop do
     puts "Elige una de las siguientes opciones: \n"
     puts    "
-            1. Agregar una persona \n
-            2. Actualizar persona \n
-            3. Eliminar persona \n
-            4. Ver cantidad de personas ingresadas \n
-            5. Mostrar comunas \n
-            6. Lista de personas entre 20 y 25 años \n
-            7. Suma de las edades de personas \n
-            8. Mostrar promedios edades \n
-            9. Mostrar lista de personas por genero \n
-            10. Salir"
+        1. Agregar una persona \n
+        2. Actualizar persona \n
+        3. Eliminar persona \n
+        4. Ver cantidad de personas ingresadas \n
+        5. Mostrar comunas \n
+        6. Lista de personas entre 20 y 25 años \n
+        7. Suma de las edades de personas \n
+        8. Mostrar promedios edades \n
+        9. Mostrar lista de personas por genero \n
+        10. Salir"
     opc = gets.chomp.to_i
     case opc
         # Opción 1: Permite ingresar los datos de una persona:
         when 1
-            puts "Ingresando una persona"
-            puts "Ingrese nombre de la persona"
-            name = gets.chomp
-            puts "Ingrese edad de la persona"
-            age = gets.chomp
-            puts "Ingrese comuna de la persona"
-            comuna = gets.chomp
-            puts "Ingrese Gènero de la persona"
-            gender = gets.chomp
-            persona[:nombre] = name
-            persona[:edad] = age
-            persona[:comuna] = comuna
-            persona[:genero] = gender
-            puts persona
         # Opción 2: Permite editar los datos de la persona.
         when 2
             puts "Editando los datos de una persona"
         # Opción 3: Permite eliminar una persona.
         when 3
             puts "Eliminando una persona"
+        #4 Debe terminar el programa.
         # Opción 4: Muestra la cantidad de personas ingresadas.
         when 4
             puts "Mostrando la cantidad de personas ingresadas"
             puts persona
-        # Opción 5: Muestra las comunas de todas las personas.
+            # Opción 5: Muestra las comunas de todas las personas.
         # hint: Generar un array con las comunas, eliminar repeticiones. Considerar que
         # pueden haber nombres escritos con mayúscula y minúscula.
         when 5
@@ -64,6 +91,6 @@ loop do
         # El menú debe repetirse hasta que el usuario ingrese la opción 10 (salir).
         when 10
             puts "Saliendo..."
-            break
+        break
     end
 end
